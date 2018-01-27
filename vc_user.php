59e625d2f26361e9d62c395be785c4ccca010b98
m-szwarc <marcinszwarc@hotmail.com>
2018-01-27 11:42:27 +0100

    Add files via upload

--- vc_user.php
+++ vc_user.php
@@ -1,12 +1,25 @@
 <?php
 /**
- * Website Version Control Script
+ * Website Version Controler
  * User-defined functions and settings
  * 
  * Version 1.0
  * Released 2018-01-26
  * 
- * License: GNU GPLv3
+ * This file is part of Website Version Controller.
+ *
+ * Website Version Controller is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Website Version Controller is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with Website Version Controller. If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright (c) 2018, Marcin Szwarc
  * 
@@ -21,6 +34,7 @@
  */
 
 define(DEFAULT_VERSION, 'default');           // Default version directory
+define(ERROR_ON_INEXISTANT_VERSION, true);   // Whether to show error when code tries to run non-existing version of site (if false, redirection to DEFAULT_VERSION will be made)
 define(VERSIONS_DIRECTORY, 'versions/');      // Directory containing versions, must end with slash
 define(ROOT_FILE, 'index.php');               // Main file for each version
 
@@ -35,12 +49,13 @@ define(DISABLE_PASS_COOKIE_NAME, 'dkey');     // Defines name of the cookie whic
 define(DISABLE_PASS_COOKIE_VALUE, '1234');    // Defines value that grants access to site with disable_partial state
 define(DISABLE_REASON, '');                   // Defines reason for disabling this website
 
-define(ENABLE_DEBUG, true);                  // Displays debug information (for example keys from cookies and selected version) on bottom of each site
+define(ENABLE_DEBUG, false);                  // Displays debug information (for example keys from cookies and selected version) on bottom of each site
+define(USE_COOKIES, true);                    // Defines whether to use cookies for making a session (if you disable cookies, no user-identifying data will be created)
 
 
 // Returns website version for the key pair ([0] => serial, [1] => random)
 function getVersionFromKeyPair($key_pair){
-  return 'default';
+  return DEFAULT_VERSION;
 }
 
 // Returns next 32-byte serial key to use is sessions
--- version_controller.php
+++ version_controller.php
@@ -1,12 +1,25 @@
 <?php
 /**
- * Website Version Control Script
+ * Website Version Controler
  * for easy version management
  *
  * Version 1.0
  * Released 2018-01-26
  *
- * License: GNU GPLv3
+ * This file is part of Website Version Controller.
+ *
+ * Website Version Controller is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Website Version Controller is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with Website Version Controller. If not, see <http://www.gnu.org/licenses/>.
  *
  * Copyright (c) 2018, Marcin Szwarc
  *
@@ -26,15 +39,17 @@ if(DISABLE_SITE == 'disable_all'){
   echo(DISABLE_REASON);
   exit;
 }
-if(DISABLE_SITE == 'disable_partial' && !isset($_COOKIE[DISABLE_PASS_COOKIE_NAME])){
-  echo('<h1>This website is disabled.</h1>');
-  echo(DISABLE_REASON);
-  exit;
-}else{
-  if(DISABLE_SITE == 'disable_partial' && $_COOKIE[DISABLE_PASS_COOKIE_NAME] != DISABLE_PASS_COOKIE_VALUE){
+if(USE_COOKIES){
+  if(DISABLE_SITE == 'disable_partial' && !isset($_COOKIE[DISABLE_PASS_COOKIE_NAME])){
     echo('<h1>This website is disabled.</h1>');
     echo(DISABLE_REASON);
     exit;
+  }else{
+    if(DISABLE_SITE == 'disable_partial' && $_COOKIE[DISABLE_PASS_COOKIE_NAME] != DISABLE_PASS_COOKIE_VALUE){
+      echo('<h1>This website is disabled.</h1>');
+      echo(DISABLE_REASON);
+      exit;
+    }
   }
 }
 
@@ -69,21 +84,47 @@ if(STATUS_PAGE_ENABLED == true && STATUS_PAGE_URL == $_GET['path']){
 }
 
 // Include root file of specified version
-$key_pair = readSessionKeyPair();
-$version = getVersionFromKeyPair($key_pair);
-if(!file_exists(VERSIONS_DIRECTORY.$version)) $version = DEFAULT_VERSION;
-if(!file_exists(VERSIONS_DIRECTORY.$version.'/'.ROOT_FILE)) $version = DEFAULT_VERSION;
+if(USE_COOKIES) $key_pair = readSessionKeyPair();
+else $key_pair = array('0000000000000000000000000000000000000000000000000000000000000000', '0000000000000000000000000000000000000000000000000000000000000000');
+if(function_exists('getVersionFromKeyPair')) $version = getVersionFromKeyPair($key_pair);
+else $version = DEFAULT_VERSION;
+if(!file_exists(VERSIONS_DIRECTORY.$version)){
+  if(ERROR_ON_INEXISTANT_VERSION){
+    echo("<h1>Requested version <code>'".$version."'</code> doesn't exist</h1>");
+    echo('Please check your system and settings and try again.');
+    echo('<hr /><i>Version Controller '.VC_VERSION.'</i> by Marcin Szwarc');
+    exit;
+  }else $version = DEFAULT_VERSION;
+}
+if(!file_exists(VERSIONS_DIRECTORY.$version.'/'.ROOT_FILE)){
+  if(ERROR_ON_INEXISTANT_VERSION){
+    echo("<h1>Requested version <code>'".$version."'</code> doesn't have a root file <code>'".ROOT_FILE."'</code></h1>");
+    echo('Please check your system and settings and try again.');
+    echo('<hr /><i>Version Controller '.VC_VERSION.'</i> by Marcin Szwarc');
+    exit;
+  }else $version = DEFAULT_VERSION;
+}
 
 if(isset($_GET['path'])) $get_path = $_GET['path']; else $get_path = '';
 
+// Include code files, flush assets (PNGs, CSSs, JSs, etc.)
 if(isAssetFile($get_path)){
-  header('Content-Type: '.getMimeTypeByFileName($get_path));
   $path = VERSIONS_DIRECTORY.$version.'/'.$get_path;
+  if(file_exists($path)) header('Content-Type: '.getMimeTypeByFileName($get_path));
+  else{
+    http_response_code(404);
+    echo("<h1>404 Not Found</h1>");
+    echo('Requested file <code>'.$get_path.'</code> wasn\'t found.');
+    echo('<hr /><i>Version Controller '.VC_VERSION.'</i> by Marcin Szwarc');
+    exit;
+  }
 }else{
   $path = VERSIONS_DIRECTORY.$version.'/'.ROOT_FILE;
 }
 
 define('CURRENT_VERSION', $version);
+define('SERIAL_KEY', $keypair[0]);
+define('RANDOM_KEY', $keypair[1]);
 chdir(realpath(VERSIONS_DIRECTORY.$version));
 
 include($path);
@@ -118,7 +159,7 @@ function generateSessionKeyPair(){
   setcookie(SERIAL_KEY_COOKIE_NAME, $skey, 0);
   setcookie(RANDOM_KEY_COOKIE_NAME, $rkey, 0);
   $key_pair = array($skey, $rkey);
-  saveSessionKeyPair($key_pair);
+  if(function_exists('saveSessionKeyPair')) saveSessionKeyPair($key_pair);
   return $key_pair;
 }
 
@@ -141,8 +182,9 @@ function readSessionKeyPair(){
     $skey = $_COOKIE[SERIAL_KEY_COOKIE_NAME];
     $rkey = $_COOKIE[RANDOM_KEY_COOKIE_NAME];
     $key_pair = array($skey, $rkey);
+    if(!function_exists('isSessionKeyPairValid')) return $key_pair;
     if(isSessionKeyPairValid($key_pair)) return $key_pair;
-    else return generateSessionKeyPair();
+    return generateSessionKeyPair();
   }else{
     return generateSessionKeyPair();
   }
